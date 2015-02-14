ff1ada62d337d2c02c09b58bdf4b234fde555b0e
jflinn18 <joeflinn412@gmail.com>
2015-02-14 12:23:45 -0800

    Completed EX1.8 do_remove_entry

--- PhoneDirectory/PD_Application.cpp
+++ PhoneDirectory/PD_Application.cpp
@@ -91,6 +91,8 @@ void do_remove_entry(Phone_Directory& the_directory) // Exercise 1.8: please com
 	cout << "Enter name: ";
 	getline(cin, name);
 	// Complete the rest of this function 
+
+	the_directory.remove_entry(name);
 	
 }
 
